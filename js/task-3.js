7d5228e5e91be65aa6486bad75656b0e6d2a4951
Stanislav-Zinovkin <stas14zinovkin@gmail.com>
2025-01-16 10:03:37 +0200

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,10 @@
+function checkForSpam(message) {
+  const ChangedMessage = message.toLowerCase();
+  return ChangedMessage.includes("spam") || ChangedMessage.includes("sale");
+}
+console.log(checkForSpam("JavaScript weekly newsletter"));
+console.log(checkForSpam("Get best sale offers now!"));
+console.log(checkForSpam("Amazing SalE, only tonight!"));
+console.log(checkForSpam("Trust me, this is not a spam message"));
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!"));
+console.log(checkForSpam("[SPAM] How to earn fast money?"));
